cdeb524b0c12b7d2b369f01f685be45eb5508ab3
Gl3nda85 <s3249319@student.rmit.edu.au>
2018-02-20 23:05:17 +1100

    fixed sidebar button (must have lost its styling in merge.)

--- app/components/Sidebar.js
+++ app/components/Sidebar.js
@@ -273,7 +273,7 @@ class Sidebar extends Component {
                 </Link>
                 :
                 <button
-                  className="stopStartButton"
+                  className="wallet-button"
                   onClick={this.startWallet}
                 >
                   {lang.startWallet}
